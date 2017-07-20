ad237e52f917f7b9f4bdbf6cf3df1b4cd6723e39
Gemfield <gemfield@civilnet.cn>
2017-07-20 10:09:59 +0800

    add download_url in setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ def readme():
     with open('README.md') as f:
         return f.read()
 
+version="0.2.0"
 setup(name='stf',
-    version='0.1.9',
+    version=version,
     description='The simple test framework',
     long_description=readme(),
     keywords='automation test',
     url='https://github.com/nokia',
+    download_url="https://github.com/nokia/STF/archive/{0}.tar.gz".format(version),
     author='Gemfield',
     author_email='gemfield@civilnet.cn',
     license='Apache License, Version 2.0',
